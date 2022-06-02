be496952e461d087c6932f8413a8607e85b0cec6
qianmianyao <904121839@qq.com>
2022-06-02 17:19:38 +0800

    修复

--- model/config.go
+++ model/config.go
@@ -3,9 +3,9 @@ package model
 import "time"
 
 type Config struct {
-	Api     string        `yaml:"api"`
-	Address []string      `yaml:"address"`
-	BotKey  string        `yaml:"botKey"`
-	BotApi  string        `yaml:"botApi"`
-	Num     time.Duration `yaml:"num"`
+	Api     string            `yaml:"api"`
+	Address map[string]string `yaml:"address"`
+	BotKey  string            `yaml:"botKey"`
+	BotApi  string            `yaml:"botApi"`
+	Num     time.Duration     `yaml:"num"`
 }
